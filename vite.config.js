2981aba20d7c47ae8b9873973183f1449f172d09
Shraddha Borse <shraddha@afreespace.com>
2025-06-15 17:21:09 +0530

    behind the scence od react understood

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import MillionLint from "@million/lint";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),            // ✅ Include React plugin
+    MillionLint.vite(),
+  ],
+});
+
